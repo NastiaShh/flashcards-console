7e3a3f77935605ceff3fb6da63d89863c641b5ed
Ibra <Ibrahim-1991@mail.ru>
2022-11-11 15:24:07 +0300

    add Question function

--- main.js
+++ main.js
@@ -29,3 +29,29 @@ function getQuestion(newDataArr, index) {
 function getAnswer(newDataArr, index) {
   return newDataArr[index].answer;
 }
+
+console.log('\x1b[33mДобро пожаловать!\n');
+
+const topics = ['1. Ястребы 🦅', '2. Выдры 🦦', '3. Еноты 🦝'];
+console.log('\x1b[37m' + topics.join('\n'));
+const readline = readlineSync.question('\x1b[34mВыберите номер темы: ');
+let score = 0;
+async function Question(readline) {
+  const fileName = (readline == 1) ? 'nighthawk_flashcard_data.txt' : (readline == 2) ? 'otter_flashcard_data.txt' : 'raccoon_flashcard_data.txt';
+
+  const questionArr = await splitFile(fileName);
+  for (let i = 0; i < questionArr.length; i += 1) {
+    console.log('\x1b[37m' + getQuestion(questionArr, i));
+    const answer = readlineSync.question('> ');
+    if (answer.toLowerCase() === getAnswer(questionArr, i)) {
+      console.log('\n\x1b[32mВерно! 👍\n');
+      score += 1;
+    } else {
+      console.log(`\n\x1b[31mНеверно! 😥\n\x1b[32mПравильный ответ:\n${getAnswer(questionArr, i)}\n`);
+    }
+  }
+
+  console.log(`\x1b[36mИтог: ${score * 10} баллов`);
+}
+
+Question(readline);
